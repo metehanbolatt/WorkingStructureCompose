c68f1c813acbb614ba2bcd156923687de5f277c4
Metehan <metehan.bolat.ie@gmail.com>
2022-09-04 02:09:51 +0300

    BackHandler

--- app/src/main/java/com/metehanbolat/workingstructurecompose/SecondScreen.kt
+++ app/src/main/java/com/metehanbolat/workingstructurecompose/SecondScreen.kt
@@ -1,5 +1,7 @@
 package com.metehanbolat.workingstructurecompose
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -21,6 +24,11 @@ fun SecondScreen() {
     ) {
         Text(text = "Settings Screen", fontSize = 50.sp)
     }
+
+    val activity = (LocalContext.current as Activity)
+    BackHandler(
+        onBack = { activity.finish() }
+    )
 }
 
 @Preview(showBackground = true)
